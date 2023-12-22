1d87c643e7a69b714a3a27b8cd9463bcbec0a8dc
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2023-12-22 19:09:26 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './styles/main.scss'
+import Home from './components/Home/home'
+import Header from './components/header/header'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <Home />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/header/header.jsx
@@ -0,0 +1,20 @@
+import './header.scss'
+
+export default function Header() {
+    return (
+        <header>
+            <section>
+                <h1>
+                    GamerHub D-S
+                </h1>
+            </section>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>GitHub</li>
+                    <li>FeedBack</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
