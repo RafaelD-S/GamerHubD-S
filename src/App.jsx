a906bd548161a59bc0aba4ab1ea21337f54f0259
RafaelD-S <rafaeldantas.vitoria@gmail.com>
2024-01-02 15:10:38 -0300

    Adição de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import './styles/main.scss'
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom' 
+
 import Home from './components/Home/home'
 import Header from './components/header/header'
+import JogoDaVelha from './pages/jogoDaVelha/jogoDaVelha'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
     <Header />
-    <Home />
+        <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/jogo-da-velha' element={<JogoDaVelha/>}/> 
+        </Routes>
+    </BrowserRouter>
     </>
   )
 }
